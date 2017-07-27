683f7919e2d3810f9e49298c67782d3e6b3ac27f
doufanther <zhengshufa@126.com>
2017-07-27 14:42:04 +0800

    原始

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <hello></hello>
+    <up :src="'/api/imgs'"></up>
+  </div>
+</template>
+
+<script>
+	import Hello from './components/Hello.vue'
+	import uploader from './components/uploader.vue'
+export default {
+  name: 'app',
+  components:{
+  	'hello':Hello,
+  	'up':uploader
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
